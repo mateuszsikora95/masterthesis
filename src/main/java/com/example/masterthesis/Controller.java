afc563290caeb935422520479435b87b4afda2f2
221050@student.pwr.edu.pl <kingpin1>
2019-05-20 23:26:30 +0200

    Change endpoint to share less data

--- src/main/java/com/example/masterthesis/Controller.java
+++ src/main/java/com/example/masterthesis/Controller.java
@@ -2,31 +2,29 @@ package com.example.masterthesis;
 
 import com.example.masterthesis.entities.Songdata;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class Controller {
 
     private SongsRepository songsRepository;
-    private RandomFileReader randomFileReader;
 
     @Autowired
-    public Controller(SongsRepository songsRepository, RandomFileReader randomFileReader) {
+    public Controller(SongsRepository songsRepository) {
         this.songsRepository = songsRepository;
-        this.randomFileReader = randomFileReader;
     }
 
     @GetMapping(path = "database")
     public List<Songdata> getCustomers() {
-        return songsRepository.findAll();
+        return songsRepository.findAll(
+                PageRequest.of((new Random()).nextInt(1750), 30)
+                ).getContent();
     }
 
-    @GetMapping(path = "file")
-    public String getText() throws IOException {
-        return randomFileReader.getRandomText();
-    }
 }
