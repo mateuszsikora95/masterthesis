84e0aa24f3b7b578dac1d138608a59fcfa35586d
221050@student.pwr.edu.pl <kingpin1>
2019-05-21 22:31:15 +0200

    Change endpoint to share less data

--- src/main/java/com/example/masterthesis/Controller.java
+++ src/main/java/com/example/masterthesis/Controller.java
@@ -1,17 +1,22 @@
 package com.example.masterthesis;
 
 import com.example.masterthesis.entities.Songdata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
 
 @RestController
 public class Controller {
+
+    Logger logger = LoggerFactory.getLogger(Controller.class);
 
     private SongsRepository songsRepository;
 
@@ -22,9 +27,12 @@ public class Controller {
 
     @GetMapping(path = "database")
     public List<Songdata> getCustomers() {
-        return songsRepository.findAll(
+        logger.debug(LocalDateTime.now().toString());
+        List<Songdata> content = songsRepository.findAll(
                 PageRequest.of((new Random()).nextInt(1750), 30)
-                ).getContent();
+        ).getContent();
+        logger.debug(LocalDateTime.now().toString());
+        return content;
     }
 
 }
